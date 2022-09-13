599a1e22331edfd248a58b3ecd2974f2fcbbc1af
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-09-13 13:32:25 +0100

    first commit

---
+++ src/main/java/com/qapitapay/instantAccount/InstantAccountApplication.java
@@ -0,0 +1,13 @@
+package com.qapitapay.instantAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qapitapay/instantAccount/InstantAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qapitapay.instantAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
